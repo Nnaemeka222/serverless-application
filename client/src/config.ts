a6b3b6ddc3494de8712c925d7bc1773523b85226
Paul <arinzennajipual@gmail.com>
2022-09-04 13:23:36 -0700

    Added my project

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'uoybr5tsv7';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-u1d8t6uw.us.auth0.com',
+  clientId: 'S2L5ACK8kDmjRtkVj1j5GrtwJkCt3qem',
+  callbackUrl: 'http://localhost:3000/callback'
+};
